40d3ee12d3e762f9cae12b28179bd88115bc7834
Yan Paulo <yanpaulo@hotmail.com>
2018-11-09 12:10:29 -0300

    Primeiro commit

---
+++ main.ts
@@ -0,0 +1,14 @@
+import Vue from "vue"
+
+let vue = new Vue({
+    el: "#app",
+    data: {
+        mensagem: "Hello, Vue!",
+        nome: ""
+    },
+    methods: {
+        clicado: function () {
+            alert("Olá, " + this.nome);
+        }
+    }
+});
